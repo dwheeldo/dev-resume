eed30120ef440e5973b539a5a854e1b2be4fdaab
David Wheeldon <47947013+dwheeldo@users.noreply.github.com>
2019-05-02 17:54:45 +0100

    Updated about section

--- src/About.js
+++ src/About.js
@@ -7,8 +7,8 @@ const About = () => {
       <p>After more than 10 years in the e-learning industry, I recently took the leap to completely focus my career on front end development.</p>
       <p>Since then I've learned JavaScript from the ground up, sharpened my knowledge of web fundamentals, and started to find my way around Node.js and React.</p>
       <p>In my working life I've proven myself to be friendly, dedicated and reliable, with great communication skills.</p>
-      <p>I've worked closely with lots of household names, including IBM, O2 and PepsiCo, and pride myself on my ability to forge strong relations with colleagues and clients alike.</p>
-      <p>I'd now relish the opportunity to take on a front end developer role. With my extensive experience of working in a production environment, I feel I have a lot to offer, and would love the chance to learn from more experienced developers while contributing to exciting new projects.</p>
+      <p>I've worked closely with lots of household names, including IBM, O2 and PepsiCo, and pride myself on my ability to build strong relations with colleagues and clients alike.</p>
+      <p>With my extensive experience of working in a production environment, I feel I have a lot to offer, and I'd love the chance to learn from more experienced developers while contributing to exciting new projects.</p>
     </div>
   )
 }
