3fc1d035065c60cf970e7c12378101fa552f8bf3
David Wheeldon <47947013+dwheeldo@users.noreply.github.com>
2019-05-04 13:37:08 +0100

    added particles background

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
 import React, { Component } from 'react';
 import Nav from './Nav';
+import Particles from 'react-particles-js';
 import NavToggler from './NavToggler';
 import PageHolder from './PageHolder';
 import './App.css';
+
+const particleSettings = {
+  "particles": {
+    "number": {
+      "value": 160,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ff8c42"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": true
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 4,
+        "size_min": 0.3,
+        "sync": false
+      }
+    },
+    "move": {
+      "enable": true,
+      "speed": 1,
+      "random": true,
+      "straight": false,
+      "out_mode": "out"
+    }
+  }
+}
 
 class App extends Component {
   constructor(props) {
@@ -44,6 +88,7 @@ class App extends Component {
     return (
       <div className="page-container">
         <Nav currentPage={ currentPage } setPage={ this.setPage } togglerOpen={ togglerOpen }/>
+        <Particles className="particles" params={particleSettings} />
         <NavToggler toggleNav={this.toggleNav} togglerOpen={ togglerOpen }/>
         <PageHolder currentPage={ currentPage } />
       </div>
