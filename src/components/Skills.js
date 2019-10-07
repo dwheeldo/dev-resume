b1b745b9aab448489264eea6d46800eb2d494a9d
dwheeldo <47947013+dwheeldo@users.noreply.github.com>
2019-10-07 21:29:04 +0100

    WIP: time for a spruce up

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,15 +6,15 @@ const Skills = () => {
       <p className="text-lead-icon"><i className="fas fa-code"></i></p>
       <p>I'm learning all the time, so this list is expanding by the day. Here's what I've got in my locker right now...</p>
       <ul>
-        <li className="css">Extensive experience building responsive web pages with HTML5 and CSS3, both from scratch and building on frameworks like Bootstrap and Tachyons</li>
+        <li className="css">Extensive experience building responsive web pages with HTML5 and Sass/CSS3, mainly from scratch, but also using boilerplate frameworks like Bootstrap</li>
         <li className="css">Experience using Sass</li>
-        <li className="js">Solid understanding of JavaScript, including DOM manipulation and API requests</li>
-        <li className="js">Experience creating single page web apps in React (I used React to build this site)</li>
-        <li className="js">Experience integrating JavaScript libraries like jQuery into projects</li>
+        <li className="js">Solid understanding of front-end JavaScript, including DOM events and API requests</li>
+        <li className="js">Experience creating single page web apps in React</li>
+        <li className="js">Experience integrating and customising JavaScript libraries</li>
         <li className="js">Basic knowledge of running a Node.js server using Express</li>
-        <li className="other">Very comfortable using Git and GitHub</li>
-        <li className="other">Comfortable using the command line and installing NPM packages</li>
-        <li className="other">I also know my way around Photoshop, MS Office and all sorts of audio production software</li>
+        <li className="other">Very comfortable managing Git repositories and GitHub</li>
+        <li className="other">Comfortable using the command line - NPM, Yarn, Gulp and WebPack</li>
+        <li className="other">I also know my way around Photoshop, XD, MS Office and all sorts of audio production software</li>
       </ul>
     </div>
   )
