efa8515b388eb7257a5129a17657e9f4507cb24a
dwheeldo <47947013+dwheeldo@users.noreply.github.com>
2020-06-03 12:00:19 +0100

    Chore: Make social links accessible

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -7,10 +7,12 @@ const socialLinks = () => {
     <div className="c-nav__socials">
       <a href="https://github.com/dwheeldo" target="_blank" rel="noopener noreferrer" className="c-nav__socials__link">
         <FontAwesomeIcon icon={faGithub} />
+        <span className="u-a11y">Github</span>
       </a>
 
       <a href="https://www.linkedin.com/in/david-wheeldon-7043b516b" target="_blank" rel="noopener noreferrer" className="c-nav__socials__link">
         <FontAwesomeIcon icon={faLinkedin} />
+        <span className="u-a11y">LinkedIn</span>
       </a>
     </div>
   )
