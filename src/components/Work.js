09735a06b5c66b5f1835193ff8216f1c7ae16c89
dwheeldo <47947013+dwheeldo@users.noreply.github.com>
2021-08-22 16:36:17 +0100

    Update work history

--- src/components/Work.js
+++ src/components/Work.js
@@ -12,12 +12,20 @@ const Work = () => {
 
       <div className="o-jobs">
         <div className="o-job">
+          <h4 className="o-job__role">Web Developer</h4>
+          <p className="o-job__date"><a href="https://electricputty.co.uk/" target="_blank" rel="noopener noreferrer" >Electric Putty</a> | October 2020 - present</p>
+          <ul className="o-job__list">
+            <li className="o-job__list__item">Creating front end components for Laravel applications using Vue.</li>
+            <li className="o-job__list__item">Handling data requests to and from REST APIs.</li>
+            <li className="o-job__list__item">Developing and maintaining Craft CMS sites.</li>
+          </ul>
+        </div>
+        <div className="o-job">
           <h4 className="o-job__role">Front End Developer</h4>
-          <p className="o-job__date"><a href="https://www.wilddogdesign.co.uk/" target="_blank" rel="noopener noreferrer" >Wild Dog Design</a> | May 2019 - present</p>
+          <p className="o-job__date"><a href="https://www.wilddogdesign.co.uk/" target="_blank" rel="noopener noreferrer" >Wild Dog Design</a> | May 2019 - October 2020</p>
           <ul className="o-job__list">
             <li className="o-job__list__item">Developing front end web pages using SCSS, JavaScript and the <a href="https://mozilla.github.io/nunjucks/" target="_blank" rel="noopener noreferrer">Nunjucks</a> templating engine</li>
             <li className="o-job__list__item">Working on <a href="https://wilddogcanvas.com/" target="_blank" rel="noopener noreferrer">Canvas</a>, a SaaS powered by Vue to generate trip itineraries for holiday companies</li>
-            <li className="o-job__list__item">Collaborating with the dev team to keep our build processes aligned with the latest standards</li>
           </ul>
         </div>
         <div className="o-job">
