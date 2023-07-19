6bc4b38c9ad9ee30452034b3a8f34e1c0afe4b97
dwheeldo <47947013+dwheeldo@users.noreply.github.com>
2023-07-19 16:39:02 +0100

    Remove unused CSS

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -4,9 +4,14 @@ const NavItem = ({ to, name, closeNav }) => {
 
   return (
     <NavLink
-      className={({ isActive }) =>
-        isActive ? "nav-item text-orange-400" : "nav-item hover:text-orange-400"
-      }
+      className={({ isActive }) => {
+        let classes = 'text-center no-underline mb-3';
+
+        if (isActive) {
+          return classes + ' text-orange-400';
+        }
+        return classes + ' hover:text-orange-400'
+      }}
       to={to}
       onClick={closeNav}
     >
