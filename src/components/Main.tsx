82ca0c4233ed71ca7add3712319ca1ab7d894e63
dwheeldo <47947013+dwheeldo@users.noreply.github.com>
2024-01-02 14:29:39 +0000

    Make heading element into a link

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,23 +1,30 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation, NavLink } from 'react-router-dom';
 import Home from '../pages/Home';
 import Skills from '../pages/Skills';
 import Work from '../pages/Work';
 import Portfolio from '../pages/Portfolio';
 import Contact from '../pages/Contact';
 import NotFound from '../pages/NotFound';
+import { useMemo } from 'react';
 
 const Main = () => {
-  const location = useLocation();
+  const { pathname } = useLocation();
 
-  const headingEl =
-    location.pathname === '/'
-      ? <h1 className='mb-1'>Dave Wheeldon</h1>
-      : <p className="h1 mb-1">Dave Wheeldon</p>;
+  const HeadingEl = useMemo(
+    () => {
+      return pathname === '/' ? 'h1' : 'p'
+    },
+    [pathname]
+  );
 
   return (
     <main className="w-full max-w-screen-md mx-auto px-4 py-8 md:p-8 lg:p-12">
       <div className="pb-4 mb-6 border-b border-orange-400 border-dashed">
-        {headingEl}
+        <NavLink to="/" className="block mb-1 no-underline" aria-label="Navigate to homepage">
+          <HeadingEl className="h1 mb-0">
+            Dave Wheeldon
+          </HeadingEl>
+        </NavLink>
         <p className="text-lg leading-none lg:text-xl">Front-end Developer</p>
       </div>
 
